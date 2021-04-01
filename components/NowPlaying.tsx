e8e243298356aa9c06c3e228efc9f185d36aa165
spin~ <spinfal@atheist.com>
2021-04-01 10:55:09 -0700

    Update NowPlaying.tsx

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -25,17 +25,18 @@ export const Player: React.FC<Props> = ({
         {`
             .paused { 
               animation-play-state: paused !important;
-              background: #e1e4e8 !important;
+              background: #47505c !important;
             }
 
             img:not([src]) {
               content: url("data:image/gif;base64,R0lGODlhAQABAPAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==");
               border-radius: 6px;
               background: #FFF;
-              border: 1px solid #e1e4e8;
+              border: 1px solid #47505c;
             }
 
             p {
+              text-transform: lowercase;
               display: block;
               opacity: 0;
             }
@@ -45,7 +46,7 @@ export const Player: React.FC<Props> = ({
               width: 100%;
               height: 4px;
               margin: -1px;
-              border: 1px solid #e1e4e8;
+              border: 1px solid #47505c;
               border-radius: 4px;
               overflow: hidden;
               padding: 2px;
@@ -59,7 +60,7 @@ export const Player: React.FC<Props> = ({
               width: 100%;
               height: 6px;
               transform-origin: left center;
-              background-color: #24292e;
+              background-color: #a7a9ab;
               animation: progress ${duration}ms linear;
               animation-delay: -${progress}ms;
             }
@@ -69,7 +70,6 @@ export const Player: React.FC<Props> = ({
             #artist,
             #cover {
               opacity: 0;
-              animation: appear 300ms ease-out forwards;
             }
 
             #track {
@@ -84,8 +84,6 @@ export const Player: React.FC<Props> = ({
             }
 
             #cover {
-              animation-name: cover-appear;
-              animation-delay: 300ms;
               box-shadow: 0 1px 3px rgba(0,0,0,0.1), 0 3px 10px rgba(0,0,0,0.05);
             }
 
@@ -147,8 +145,8 @@ export const Player: React.FC<Props> = ({
           <Text id="track" weight="bold">
             {`${track ?? ""} `.trim()}
           </Text>
-          <Text id="artist" color={!track ? "gray" : undefined}>
-            {artist || "Nothing playing..."}
+          <Text id="artist" color={!track ? "white" : undefined}>
+            {artist || "spin isnt using spotify..."}
           </Text>
           {track && (
             <div className="progress-bar">
